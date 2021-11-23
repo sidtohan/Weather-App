9c87d59d632012c6269075640592b5762f91b6c1
sidtohan <siddhant.tohan@gmail.com>
2021-11-23 11:13:35 +0530

    Finished the footer section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import apiHandler from "./apiHandler";
 import formHandler from "./formHandler";
+import "./stylesheets/home.css";
 
 // passing the apiHandler object into formHandler directly
 formHandler(apiHandler);
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
