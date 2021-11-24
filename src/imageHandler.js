08b96ac5344cfe9065740c7324fc10bd786b71fc
sidtohan <siddhant.tohan@gmail.com>
2021-11-24 15:29:49 +0530

    Made some more changes to the weather icons

--- src/imageHandler.js
+++ src/imageHandler.js
@@ -1,16 +1,22 @@
 import Haze from "./assets/weather/haze.svg";
 import Cloudy from "./assets/weather/cloudy.svg";
+import Rain from "./assets/weather/rain.svg";
+import Clear from "./assets/weather/clear.svg";
+import Drizzle from "./assets/weather/drizzle.svg";
+import Thunderstorm from "./assets/weather/thunderstorm.svg"
+
 import Humidity from "./assets/humidity.svg";
 import Wind from "./assets/wind.svg";
-
 const imageHandler = () => {
   const weatherIcon = (data) => {
-    if (data == "Haze") {
-      return Haze;
-    } else {
-      return Cloudy;
-    }
+    if (data == "Haze") return Haze;
+    else if (data == "Rain") return Rain;
+    else if (data == "Clear") return Clear;
+    else if (data == "Clouds") return Cloudy;
+    else if (data == "Drizzle") return Drizzle;
+    else return Thunderstorm
   };
+
   const getHumidityImage = () => {
     return Humidity;
   };
