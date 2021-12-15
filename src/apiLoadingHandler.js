1f26704fa8b2ed1229d6deeaa44ef7be41f65a02
sidtohan <siddhant.tohan@gmail.com>
2021-12-15 18:46:41 +0530

    Made some adjustments to the loading screen, now works perfectly

--- src/apiLoadingHandler.js
+++ src/apiLoadingHandler.js
@@ -3,13 +3,14 @@ const apiHandler = (() => {
   const apiKey = "94a2aad4a38121e794735a95ee415a76";
 
   async function callApi(cityName) {
+    document.body.appendChild(loadingHandler.returnLoading());
     const response = await fetch(apiLink + `q=${cityName}&appid=${apiKey}`, {
       mode: "cors",
     });
     if (!response.ok) {
+      document.body.removeChild(document.querySelector(".loading-begin"));
       throw new Error(response.status);
     }
-    document.body.appendChild(loadingHandler.returnLoading());
     const data = await response.json();
     return data;
   }
