8ddc71334647ad3fc2f7e2595e3302946092c06e
sidtohan <siddhant.tohan@gmail.com>
2022-04-16 10:03:57 +0530

    adjusted font size

--- src/utils/iconMapper.js
+++ src/utils/iconMapper.js
@@ -48,14 +48,15 @@ const iconVariants = {
   hidden: {
     scale: 0,
     opacity: 0,
+    transition: {
+      duration: 0.2,
+    },
   },
   visible: {
     scale: 1,
     opacity: 1,
     transition: {
-      duration: 1,
-      type: "spring",
-      stiffness: 150,
+      duration: 0.2,
     },
   },
 };
